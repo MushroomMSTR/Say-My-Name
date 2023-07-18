d904409ff38aab8b643dfb889d6d08767d36debd
NazarStf <hworker98@gmail.com>
2023-07-18 12:27:07 +0300

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by NazarStf on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
