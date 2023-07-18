dc9db8c0cbaea28dbe3911cd7ad40e7298b9ef62
NazarStf <hworker98@gmail.com>
2023-07-18 22:15:34 +0300

    Lesson 12 - Finishing Touches

--- BB Quotes/View/QuoteView.swift
+++ BB Quotes/View/QuoteView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct QuoteView: View {
 	
 	@StateObject private var viewModel = ViewModel(controller: FetchController())
+	@State private var showCharacterInfo = false
 	let show: String
 	
 	var body: some View {
@@ -42,6 +43,12 @@ struct QuoteView: View {
 									ProgressView()
 								}
 								.frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+								.onTapGesture {
+									showCharacterInfo.toggle()
+								}
+								.sheet(isPresented: $showCharacterInfo) {
+									CharacterView(show: show, character: data.character)
+								}
 								
 								Text(data.quote.character)
 									.foregroundColor(.white)
